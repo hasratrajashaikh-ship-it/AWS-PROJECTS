39753a0dedb816ecb4d8ac7c3e9b6123d20b9c19
Hasrat raja Shaikh <hasratraja.shaikh@adypu.edu.in>
2026-03-12 15:31:23 +0530

    Update EC2-main.tf

--- Terraform AWS-EC2 and S3/EC2-main.tf
+++ Terraform AWS-EC2 and S3/EC2-main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "eu-north-1"
+}
+
+
+resource "aws_instance" "myserver" {
+  ami           = "ami-0c0e147c706360bd7"
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "SampleServer"
+  }
+}
